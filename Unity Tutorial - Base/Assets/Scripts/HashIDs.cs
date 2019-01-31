a1515e313f69c6b4583d066dce839878de1c93dd
Boyceinfenwa <Kieran2.Boyce@live.uwe.ac.uk>
2019-01-31 10:48:36 +0000

    Unity stuff
    Worksheet 1 80% done

---
+++ Unity Tutorial - Base/Assets/Scripts/HashIDs.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashIDs : MonoBehaviour
+{
+    public int dyingState;
+    public int deadBool;
+    public int walkState;
+    public int shoutingBool;
+    public int shoutState;
+    public int sneakingBool;
+    public int speedFloat;
+
+    // Use this for initialization
+	void Start ()
+    {
+    
+    }
+
+   private void Awake()
+    {
+        dyingState = Animator.StringToHash("Base Layer Dying");
+        deadBool = Animator.StringToHash("Dead");
+        walkState = Animator.StringToHash("Walk");
+        shoutingBool = Animator.StringToHash("Shouting");
+        shoutState = Animator.StringToHash("Shouting.Shout");
+        speedFloat = Animator.StringToHash("Speed");
+        sneakingBool = Animator.StringToHash("Sneaking");
+    }
+
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
---
+++ Unity Tutorial - Base/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public AudioClip shoutingClip;
+    public float speedDamptime = 0.0f;
+    public float xSensitivity = 1.0f;
+
+    private Animator anim;
+    private HashIDs hash;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator> () ;
+        hash = GameObject.FindGameObjectWithTag("GameController").GetComponent<HashIDs>();
+        anim.SetLayerWeight(1, 1f);
+    }
+
+    private void FixedUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        bool sneak = Input.GetButton("Sneak");
+        float mouse_x = Input.GetAxis("MouseX");
+
+    }
+}
